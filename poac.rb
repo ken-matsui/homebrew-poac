6fc52f5d40d48aa975850a9fa16cdf2792621e79
matken11235 <matken11235@gmail.com>
2018-09-07 04:35:06 +0900

    Fix: C: 8: col 1: Extra blank line detected.

--- poac.rb
+++ poac.rb
@@ -5,7 +5,6 @@ class Poac < Formula
       :tag => "0.0.1-beta",
       :revision => "16d42cf3529995689fda12b2f16f1fae20ea23d5"
 
-
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "curl"
