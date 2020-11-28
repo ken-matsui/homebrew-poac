51830c2280014e626c36819c45022884d9a9f26d
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-11-28 17:39:30 +0900

    Update poac.rb

--- poac.rb
+++ poac.rb
@@ -10,7 +10,8 @@ class Poac < Formula
   depends_on "llvm" if MacOS.version <= :high_sierra
   depends_on :macos => :sierra
   depends_on "openssl"
-  depends_on "yaml-cpp"
+  depends_on "libgit2"
+  depends_on "poacpm/toml11/toml11"
 
   def install
     mkdir "bulid" do
