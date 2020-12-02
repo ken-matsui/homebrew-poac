ee8c902380eef27167b184fde31d129c3b6829c2
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-03 02:09:06 +0900

    Update poac.rb

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -1,16 +1,15 @@
 class Poac < Formula
   desc "Package manager for C++"
   homepage "https://github.com/poacpm"
-  url "https://github.com/poacpm/poac.git",
-      :tag      => "0.1.2",
-      :revision => "2a38827050cd797827c8b6154240454ffc518d53"
+  head "https://github.com/poacpm/poac.git"
 
   depends_on "cmake" => :build
   depends_on "boost"
   depends_on "llvm" if MacOS.version <= :high_sierra
   depends_on :macos => :sierra
   depends_on "openssl"
-  depends_on "yaml-cpp"
+  depends_on "poacpm/tap/toml11"
+  depends_on "poacpm/tap/clipp"
 
   def install
     mkdir "bulid" do
@@ -25,6 +24,6 @@ class Poac < Formula
   end
 
   test do
-    assert_match /Usage/, shell_output("#{bin}/poac --help")
+    assert_match /SYNOPSIS/, shell_output("#{bin}/poac --help")
   end
 end
