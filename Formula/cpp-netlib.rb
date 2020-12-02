a648ab22cec4412657eb81918db21d47964cbb35
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-03 01:58:01 +0900

    Create cpp-netlib.rb

---
+++ Formula/cpp-netlib.rb
@@ -0,0 +1,19 @@
+class CppNetlib < Formula
+  desc "The C++ Network Library Project -- cross-platform, standards compliant networking library."
+  homepage "http://cpp-netlib.org"
+  url "https://github.com/cpp-netlib/cpp-netlib.git"
+
+  depends_on "cmake" => :build
+  depends_on "boost"
+  
+  def install
+    mkdir "bulid" do
+      system "cmake", "..", "-DCPP-NETLIB_BUILD_TESTS=OFF", "-DCPP-NETLIB_BUILD_EXAMPLES=OFF", *std_cmake_args
+      system "make", "install"
+    end
+  end
+
+  test do
+    assert_predicate "/usr/local/lib/libcppnetlib-server-parsers.a", :exist?
+  end
+end
