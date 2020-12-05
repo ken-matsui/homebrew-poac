6e65fb895916245ebb53b9e1cae32bba55cb759c
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-06 04:51:01 +0900

    Update plog.rb

--- Formula/plog.rb
+++ Formula/plog.rb
@@ -13,30 +13,6 @@ class Plog < Formula
   end
 
   test do
-#     (testpath/"test.cpp").write <<~EOS
-#       #include <plog/Log.h> // Step1: include the headers
-#       #include <plog/Initializers/RollingFileInitializer.h>
-
-#       int main()
-#       {
-#           plog::init(plog::debug, "Hello.txt"); // Step2: initialize the logger
-
-#           // Step3: write log messages using a special macro
-#           // There are several log macros, use the macro you liked the most
-
-#           PLOGD << "Hello log!"; // short macro
-#           PLOG_DEBUG << "Hello log!"; // long macro
-#           PLOG(plog::debug) << "Hello log!"; // function-style macro
-    
-#           // Also you can use LOG_XXX macro but it may clash with other logging libraries
-#           LOGD << "Hello log!"; // short macro
-#           LOG_DEBUG << "Hello log!"; // long macro
-#           LOG(plog::debug) << "Hello log!"; // function-style macro
-
-#           return 0;
-#       }
-#     EOS
-#     system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
-#     system "./test"
+    system "cat /usr/local/include/plog/Log.h"
   do
 end
