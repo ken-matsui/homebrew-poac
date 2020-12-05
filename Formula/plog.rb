08661d5f63650e1e746b20dfcfbdcebf55ba7009
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2020-12-06 04:56:14 +0900

    Update plog.rb

--- Formula/plog.rb
+++ Formula/plog.rb
@@ -2,9 +2,9 @@ class Plog < Formula
   desc "Portable, simple and extensible C++ logging library"
   homepage "https://github.com/SergiusTheBest/plog"
   url "https://github.com/SergiusTheBest/plog.git", :tag => "1.1.5"
-  
+
   depends_on "cmake" => :build
-  
+
   def install
     mkdir "bulid" do
       system "cmake", "..", *std_cmake_args
@@ -13,6 +13,29 @@ class Plog < Formula
   end
 
   test do
-    system "cat /usr/local/include/plog/Log.h"
-  do
+    (testpath/"test.cpp").write <<~EOS
+      #include <plog/Log.h> // Step1: include the headers
+      #include <plog/Initializers/RollingFileInitializer.h>
+      int main()
+      {
+          plog::init(plog::debug, "Hello.txt"); // Step2: initialize the logger
+
+          // Step3: write log messages using a special macro
+          // There are several log macros, use the macro you liked the most
+
+          PLOGD << "Hello log!"; // short macro
+          PLOG_DEBUG << "Hello log!"; // long macro
+          PLOG(plog::debug) << "Hello log!"; // function-style macro
+
+          // Also you can use LOG_XXX macro but it may clash with other logging libraries
+          LOGD << "Hello log!"; // short macro
+          LOG_DEBUG << "Hello log!"; // long macro
+          LOG(plog::debug) << "Hello log!"; // function-style macro
+
+          return 0;
+      }
+    EOS
+    system ENV.cxx, "test.cpp", "-std=c++11", "-o", "test"
+    system "./test"
+  end
 end
