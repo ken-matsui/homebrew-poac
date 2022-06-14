a6caa0d5a7f8bab62dda4b842a9d5f948c7ebf96
Ken Matsui <26405363+ken-matsui@users.noreply.github.com>
2022-06-14 19:55:26 +0900

    Bump up Poac to 0.3.4 (#31)
    * Test HEAD on ubuntu CI
    
    * Update poac.rb
    
    * Update homebrew.yml
    
    * Update poac.rb
    
    * Update poac.rb

--- Formula/poac.rb
+++ Formula/poac.rb
@@ -2,15 +2,13 @@ class Poac < Formula
   desc "Package manager for C++"
   homepage "https://github.com/poacpm/poac"
   url "https://github.com/poacpm/poac.git",
-    tag:      "0.3.3",
-    revision: "788bbbe12e56be1bbd7204baf3c77ed8879499bf"
+    tag:      "0.3.4",
+    revision: "f905a175ccd1b9ad94041e1e0a29d12d92e8308d"
   license "Apache-2.0"
   head "https://github.com/poacpm/poac.git", branch: "main"
 
   depends_on "cmake" => :build
   depends_on "boost"
-  depends_on "libarchive"
-  depends_on "libgit2"
   depends_on macos: :big_sur # C++20
   depends_on "openssl@3"
 
@@ -21,7 +19,7 @@ class Poac < Formula
 
   def install
     mkdir "build" do
-      system "cmake", "..", "-DCPM_USE_LOCAL_PACKAGES=ON", *std_cmake_args
+      system "cmake", "..", *std_cmake_args
       system "make", "install"
     end
 
